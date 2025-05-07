cb1ef2863dcff4ed579f42908171bb59525e6a72
Usatblu27 <daniilusatenko2772@gmail.com>
2025-05-07 10:24:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = "my-game-cache-v1";
 const urlsToCache = [
-    "/",
-    "index.html",
-    "styles.css",
-    "script.js",
-    "icon-192x192.png"
+  '/draword/',
+  '/draword/index.html',
+  '/draword/styles.css',
+  '/draword/script.js',
+  '/draword/icon-192x192.png'
 ];
 
 self.addEventListener("install", (event) => {
